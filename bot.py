261bd6fcb3283b74a673939ca1f46168f8f23947
enicely <mail2emn@gmail.com>
2023-03-07 19:29:30 -0600

    remove newline

--- bot.py
+++ bot.py
@@ -49,7 +49,6 @@ client.hist = {}
 
 client.last_response_time = datetime.now() - timedelta(minutes=COOLDOWN + 1)
 
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
